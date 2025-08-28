8edee9debb494ad027b6728df1ec297488b253ef
trekkie3 <228154489+trekkie3@users.noreply.github.com>
2025-08-28 02:56:04 -0400

    Reformat code

--- src/main/java/Command.java
+++ src/main/java/Command.java
@@ -4,24 +4,24 @@ public class Command {
 
     public Command(String line) {
         switch (line) {
-            case "list" -> this.type = CommandType.LIST;
-            case "bye" -> this.type = CommandType.BYE;
-            default -> {
-                String[] split = line.split(" ", 2);
-                String command = split[0];
-                this.maybeArgument = (
-                        split.length > 1 ? split[1] : null
-                );
-                switch (command) {
-                    case "delete" -> this.type = CommandType.DELETE;
-                    case "mark" -> this.type = CommandType.MARK;
-                    case "unmark" -> this.type = CommandType.UNMARK;
-                    case "todo" -> this.type = CommandType.ADD_TODO;
-                    case "deadline" -> this.type = CommandType.ADD_DEADLINE;
-                    case "event" -> this.type = CommandType.ADD_EVENT;
-                    default -> this.type = CommandType.INVALID;
-                }
+        case "list" -> this.type = CommandType.LIST;
+        case "bye" -> this.type = CommandType.BYE;
+        default -> {
+            String[] split = line.split(" ", 2);
+            String command = split[0];
+            this.maybeArgument = (
+                    split.length > 1 ? split[1] : null
+            );
+            switch (command) {
+            case "delete" -> this.type = CommandType.DELETE;
+            case "mark" -> this.type = CommandType.MARK;
+            case "unmark" -> this.type = CommandType.UNMARK;
+            case "todo" -> this.type = CommandType.ADD_TODO;
+            case "deadline" -> this.type = CommandType.ADD_DEADLINE;
+            case "event" -> this.type = CommandType.ADD_EVENT;
+            default -> this.type = CommandType.INVALID;
             }
+        }
         }
     }
 }
--- src/main/java/task/TaskException.java
+++ src/main/java/task/TaskException.java
@@ -2,6 +2,7 @@ package task;
 
 public class TaskException extends Exception {
     final TaskExceptionType exceptionType;
+
     TaskException(TaskExceptionType exceptionType) {
         this.exceptionType = exceptionType;
     }
