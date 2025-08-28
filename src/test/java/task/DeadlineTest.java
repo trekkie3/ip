49c5ceef7ed6c98b0f87c926ce79f14110880cef
trekkie3 <228154489+trekkie3@users.noreply.github.com>
2025-08-28 02:26:21 -0400

    Add Deadline and Event tests

---
+++ src/test/java/task/DeadlineTest.java
@@ -0,0 +1,24 @@
+package task;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DeadlineTest {
+
+    @Test
+    public void testGenerateDeadline_success() throws TaskException {
+        Deadline event = Deadline.generateDeadline("Project meeting /by 2024-06-15");
+        assertEquals("[D][ ] Project meeting (by: Jun 15 2024)", event.toString());
+    }
+
+    @Test
+    public void testGenerateDeadline_exception() {
+        try {
+            Deadline.generateDeadline("Project meeting");
+            fail();
+        } catch (TaskException exception) {
+            assertSame(TaskExceptionType.ARGUMENTS_MISSING, exception.exceptionType);
+        }
+    }
+}
---
+++ src/test/java/task/EventTest.java
@@ -0,0 +1,26 @@
+package task;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class EventTest {
+
+    @Test
+    public void testGenerateEvent_success() throws TaskException {
+        Event event = Event.generateEvent("Project meeting /from 2024-06-15 /to 2024-06-18");
+        assertEquals("[E][ ] Project meeting (from: Jun 15 2024 to: Jun 18 2024)", event.toString());
+    }
+
+    @Test
+    public void testGenerateEvent_exception() {
+        try {
+            Event.generateEvent("Project meeting /from 2024-06-15");
+            fail();
+        } catch (TaskException exception) {
+            assertSame(TaskExceptionType.ARGUMENTS_MISSING, exception.exceptionType);
+        }
+    }
+}
