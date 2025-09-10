1da2e72a54d8a0acfb47065e4e5557dcfc283fe2
trekkie3 <228154489+trekkie3@users.noreply.github.com>
2025-09-10 06:43:27 -0400

    Magic string avoidance

--- src/main/java/iris/Command.java
+++ src/main/java/iris/Command.java
@@ -1,5 +1,7 @@
 package iris;
 
+import java.util.Map;
+
 /**
  * Represents a command given by the user.
  */
@@ -7,31 +9,29 @@ public class Command {
     final CommandType type;
     private String maybeArgument = null;
 
+    // Map of command keywords to their corresponding types to avoid magic strings
+    private static final Map<String, CommandType> COMMANDS = Map.of(
+        "list", CommandType.LIST,
+        "bye", CommandType.BYE,
+        "find", CommandType.FIND,
+        "delete", CommandType.DELETE,
+        "mark", CommandType.MARK,
+        "unmark", CommandType.UNMARK,
+        "todo", CommandType.ADD_TODO,
+        "deadline", CommandType.ADD_DEADLINE,
+        "event", CommandType.ADD_EVENT
+    );
+
     /**
      * Creates a iris.Command object from a line of input.
      *
      * @param line input line
      */
     public Command(String line) {
-        switch (line) {
-        case "list" -> this.type = CommandType.LIST;
-        case "bye" -> this.type = CommandType.BYE;
-        default -> {
-            String[] split = line.split(" ", 2);
-            String command = split[0];
-            this.maybeArgument = split.length > 1 ? split[1] : null;
-            switch (command) {
-            case "find" -> this.type = CommandType.FIND;
-            case "delete" -> this.type = CommandType.DELETE;
-            case "mark" -> this.type = CommandType.MARK;
-            case "unmark" -> this.type = CommandType.UNMARK;
-            case "todo" -> this.type = CommandType.ADD_TODO;
-            case "deadline" -> this.type = CommandType.ADD_DEADLINE;
-            case "event" -> this.type = CommandType.ADD_EVENT;
-            default -> this.type = CommandType.INVALID;
-            }
-        }
-        }
+        String[] split = line.split(" ", 2);
+        String command = split[0];
+        this.maybeArgument = split.length > 1 ? split[1] : null;
+        this.type = COMMANDS.getOrDefault(command, CommandType.INVALID);
     }
 
     public String getMaybeArgument() {
